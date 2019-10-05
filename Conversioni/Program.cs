3b6b6d7765ad0db193d8c53218b3c59fa38b5f06
studenti <studenti@rosselli.loc>
2019-10-05 11:15:41 +0200

    Add project files.

---
+++ Conversioni/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Conversioni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int decimale = 0;
+            Console.Write("Inserisci un numero decimale: ");
+            decimale = int.Parse(Console.ReadLine());
+            string binary = (Convert.ToString(decimale, 2));
+            Console.Write($"Il numero decimale in binario è {binary}");
+            Console.ReadLine();
+        }
+    }
+}
