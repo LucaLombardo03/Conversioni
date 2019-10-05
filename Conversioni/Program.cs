063deb54f48bc0be7370babd82cef834ed2195c0
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-05 11:50:50 +0200

    Add method (ToBinary, ToHexadecimal, ToOctal)

--- Conversioni/Program.cs
+++ Conversioni/Program.cs
@@ -9,9 +9,40 @@ namespace Conversioni
             int decimale = 0;
             Console.Write("Inserisci un numero decimale: ");
             decimale = int.Parse(Console.ReadLine());
-            string binary = (Convert.ToString(decimale, 2));
-            Console.Write($"Il numero decimale in binario è {binary}");
+            string risposta;
+            Console.WriteLine("Come vuoi convertire il numero decimale? (binary/hexadecimal/octal/esci)");
+            risposta = Console.ReadLine();
+            string binarynumber;
+            string hexadecimalnumber;
+            string octalnumber;
+            try
+            {
+                if (risposta == "binary")
+                {
+                    binarynumber = LibreriaConversioni.LibreriaConversioni.ToBinary(decimale);
+                    Console.Write($"Il numero decimale in binario è {binarynumber}");
+                }
+                else if (risposta == "hexadecimal")
+                {
+                    hexadecimalnumber = LibreriaConversioni.LibreriaConversioni.ToHexadecimal(decimale);
+                    Console.Write($"Il numero decimale in esadecimale è {hexadecimalnumber}");
+                }
+                else if (risposta == "octal")
+                {
+                    octalnumber = LibreriaConversioni.LibreriaConversioni.ToOctal(decimale);
+                    Console.Write($"Il numero decimale in ottale è {octalnumber}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            if (risposta == "esci")
+            {
+                Console.WriteLine("Ciao");
+            }
             Console.ReadLine();
         }
+        
     }
 }
---
+++ LibreriaConversioni/LibreriaConversioni.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LibreriaConversioni
+{
+    public class LibreriaConversioni
+    {
+        public static string ToBinary(int decimale)
+        {
+            string binarynumber = (Convert.ToString(decimale, 2));
+            return binarynumber;
+        }
+        public static string ToHexadecimal(int decimale)
+        {
+            string hexadecimalnumber = (Convert.ToString(decimale, 16));
+            return hexadecimalnumber;
+        }
+        public static string ToOctal(int decimale)
+        {
+            string octalnumber = (Convert.ToString(decimale, 8));
+            return octalnumber;
+        }
+    }
+}
